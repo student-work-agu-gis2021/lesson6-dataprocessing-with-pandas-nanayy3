435e926b9ea6d8520fb90007dedd857d74966d56
nanayy3 <>
2021-05-23 14:42:18 +0000

    I found the average temperature for the whole data file from column

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -99,6 +99,7 @@ print('Date of the last observation:', last_obs)
 avg_temp = None
 
 # YOUR CODE HERE 7
+avg_temp = np.mean(data.iloc[:, 6])
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
