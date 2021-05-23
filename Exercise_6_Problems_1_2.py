6a58b7aed1dac2e1111fdcc195f831b8b4913f25
nanayy3 <>
2021-05-23 15:13:17 +0000

    I calculated the average "TMAX" temperatures over the summer of 1969.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -113,6 +113,7 @@ print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
 avg_temp_1969 = None
 
 # YOUR CODE HERE 8
+avg_temp_1969 = np.mean(data[19690500 < data.col4 < 19690900].iloc[:,7])
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print a number
