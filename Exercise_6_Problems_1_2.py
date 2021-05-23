9aa5c8242f1bf85c121ac2dfe96459617f162449
nanayy3 <>
2021-05-23 14:38:10 +0000

    I counted the total number days covered by this data file.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -58,6 +58,7 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
+day_count = len(data)
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
